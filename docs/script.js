a832fb5c52bf2f210e92e9ab26193f13291d548f
Nagarjuna Chenna <121895209+nagarjuna06@users.noreply.github.com>
2023-01-26 23:34:26 +0530

    addind source files

---
+++ docs/script.js
@@ -0,0 +1,23 @@
+let qrCode = new QRCode(document.querySelector('.qr-code'))
+qrCode.makeCode("nagarjuna06.github.com")
+let qrCodeContainer = document.querySelector(".qr-code-container")
+let input = document.querySelector(".input");
+var link = document.querySelector("a");
+function generateQr(){
+    qrCode.makeCode(input.value)
+    downloadimage()
+    link.classList.remove('d-none')
+
+}
+
+input.addEventListener("input",generateQr)
+function downloadimage() {
+    var container = document.querySelector(".qr-code-cont"); /* full page */
+    html2canvas(container, { allowTaint: true }).then(function (canvas) {
+
+        link.download = "qr-code.jpg";
+        link.href = canvas.toDataURL();
+        link.target = '_blank';
+    });
+}
+link.click();
